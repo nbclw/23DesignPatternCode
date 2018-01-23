be1917a68d7b5297b22c8abd599e819b1e2126c8
nbclw <1027477807@qq.com>
2018-01-23 09:41:00 +0800

    11

---
+++ Builder/House.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    public abstract class House
+    {
+        public abstract void BuildDoor();
+        public abstract void BuildWall();
+        public abstract void BuildWindows();
+        public abstract void BuildFloor();
+        public abstract void BuildHouseCeiling();
+
+        public abstract House GetHouse();
+    }
+}
